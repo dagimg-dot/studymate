636dda0e4bdadb1c62fffe822bedee621e3ff27c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-04-14 08:16:25 +0300

    update the chat widget class

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 // import 'package:flutter/src/widgets/placeholder.dart';
@@ -16,14 +17,16 @@ class ChatWidget extends StatelessWidget {
     return Column(
       children: [
         Material(
-          color: chatIndex == 0 ? scaffoldBackgroundColor: cardColor,
+          color: chatIndex == 0 ? scaffoldBackgroundColor : cardColor,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start ,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Image.asset(
-                  chatIndex == 0 ? AssetsManager.userImage: AssetsManager.botImage,
+                  chatIndex == 0
+                      ? AssetsManager.userImage
+                      : AssetsManager.botImage,
                   height: 30,
                   width: 30,
                 ),
@@ -31,25 +34,47 @@ class ChatWidget extends StatelessWidget {
                   width: 8,
                 ),
                 Expanded(
-                  child: TextWidget(
-                    label: msg,
-                  ),
+                  child: chatIndex == 0
+                      ? TextWidget(
+                          label: msg,
+                        )
+                      : DefaultTextStyle(
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                          child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              repeatForever: false,
+                              displayFullTextOnTap: true,
+                              totalRepeatCount: 1,
+                              animatedTexts: [
+                                TyperAnimatedText(
+                                  msg.trim(),
+                                )
+                              ]),
+                        ),
                 ),
-                chatIndex == 0 ? const SizedBox.shrink(): Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Icon(
-                      Icons.thumb_up_alt_outlined,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 5,),
-                    Icon(
-                      Icons.thumb_down_alt_outlined,
-                      color: Colors.white,
-                    ),
-                  ],
-                )  
+                chatIndex == 0
+                    ? const SizedBox.shrink()
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(
+                            Icons.thumb_up_alt_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.thumb_down_alt_outlined,
+                            color: Colors.white,
+                          ),
+                        ],
+                      )
               ],
             ),
           ),
